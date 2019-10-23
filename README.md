20689507cd2891f24e9c6e45180bb11aea5c7954
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-10-23 14:17:05 -0400

    readme, headers, files

---
+++ README.md
@@ -0,0 +1,3 @@
+# MKS65 - list
+### Kiran Vuksanaj
+W03
---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include"linkedlist.h"
+
+int main(){
+  printf("Linked List! W03\nKiran Vuksanaj\n");
+  return 0;
+}
