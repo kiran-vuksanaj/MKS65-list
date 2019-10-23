df983d0c19bdc54b6124f71602bd5c00bb4873fc
Kiran Vuksanaj <kiran-vuksanaj@gmail.com>
2019-10-23 15:26:04 -0400

    added return values, test cases

--- linkedlist.c
+++ linkedlist.c
@@ -15,6 +15,7 @@ struct node * insert_front(struct node * list, int val){
   struct node * out = malloc(sizeof (struct node) );
   out -> val = val;
   out -> next = list;
+  return out;
   // puts this in front of the list
   // if null is passed in for the existing list, a new list is born!
 }
@@ -23,6 +24,8 @@ struct node * free_list(struct node * list) {
   if( list != NULL ) {
     //recursive: results in last element being freed first
     free_list( list -> next );
+    printf("\t[freeing node with value %d]\n",list -> val);
     free(list);
   }
+  return NULL;
 }
--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 #include"linkedlist.h"
 
 int main(){
   printf("Linked List! W03\nKiran Vuksanaj\n");
+  time_t seed = time(NULL);
+  printf("Seed: %lu\n",seed);
+  srand(seed);
+
+  printf("Print empty list: ");
+  print_list(NULL);
+
+  size_t listsize = rand() % 25;
+  printf("Generating random list of size %lu\n",listsize);
+
+  struct node * list = NULL;
+  size_t i;
+  for( i = 0; i < listsize; i++ ) {
+    int val = rand() % 100 - 50;
+    printf("\tInserting %d\n",val);
+    list = insert_front(list,val);
+  }
+
+  printf("Print generated list: ");
+  print_list(list);
+
+  printf("Free list: \n");
+  list = free_list(list);
+
+  printf("Print emptied list: ");
+  print_list(list);
   return 0;
 }
