0caf588777f5c072097131874417cdcaa6f82ab0
Kiran Vuksanaj <kiran-vuksanaj@gmail.com>
2019-10-23 14:49:35 -0400

    theoretical insert_front and free_list

--- linkedlist.c
+++ linkedlist.c
@@ -3,7 +3,7 @@
 #include"linkedlist.h"
 
 void print_list( struct node *node ) {
-  printf("[ ")
+  printf("[ ");
   while(node != 0) { // last element's next will be null
     printf("%d ",node -> val);
     node = node -> next;
@@ -11,5 +11,18 @@ void print_list( struct node *node ) {
   printf("]\n");
 }
 
-struct node * insert_front(struct node * list, int val);
-struct node * free_list(struct node * list);
+struct node * insert_front(struct node * list, int val){
+  struct node * out = malloc(sizeof (struct node) );
+  out -> val = val;
+  out -> next = list;
+  // puts this in front of the list
+  // if null is passed in for the existing list, a new list is born!
+}
+
+struct node * free_list(struct node * list) {
+  if( list != NULL ) {
+    //recursive: results in last element being freed first
+    free_list( list -> next );
+    free(list);
+  }
+}
