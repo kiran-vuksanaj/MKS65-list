9b58a15a55ef5056046b34ddcaf4333b9f39991a
Kiran Vuksanaj <kiran-vuksanaj@gmail.com>
2019-10-23 14:36:53 -0400

    theoretical print list

--- linkedlist.c
+++ linkedlist.c
@@ -1,3 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"linkedlist.h"
+
+void print_list( struct node *node ) {
+  printf("[ ")
+  while(node != 0) { // last element's next will be null
+    printf("%d ",node -> val);
+    node = node -> next;
+  }
+  printf("]\n");
+}
+
+struct node * insert_front(struct node * list, int val);
+struct node * free_list(struct node * list);
--- linkedlist.h
+++ linkedlist.h
@@ -1,10 +1,13 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
+
 struct node {
   int val;
   struct node *next;
 };
+
 void print_list( struct node *list );
 struct node * insert_front(struct node * list, int val);
 struct node * free_list(struct node * list);
+
 #endif
